b0b1a26b6672e7ee78e7bf0506b931911d795307
vecerniv <vecerniv@vscht.cz>
2025-04-26 20:08:33 +0200

    fixed typing in plot_utils

--- lutils/plotting/plot_utils.py
+++ lutils/plotting/plot_utils.py
@@ -7,14 +7,14 @@ from typing import Callable, Union
 
 
 # returns rows from df corresponding to input value
-def values_in_range(df: pd.DataFrame, col: str, start: float, stop: float) -> None:
+def values_in_range(df: pd.DataFrame, col: str, start: float, stop: float) -> pd.DataFrame:
         lower = bisect.bisect_left(df[col], start)
         higher = bisect.bisect_right(df[col], stop)
         return df.loc[lower:higher-1, :].copy()
 
 # return ys of an outline based on xs
 def outline(fig: Union[int, str, matplotlib.figure.Figure, matplotlib.figure.SubFigure],
-            x: pd.Series, func: Callable) -> None:
+            x: pd.Series, func: Callable[[float], float]) -> None:
     plt.figure(fig)
     y = x.sort_values().apply(func)
     plt.plot(x.sort_values(), y, color="black")
